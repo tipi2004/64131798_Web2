2d62bf3cce4de835388e31ad2ceb8d219beb7317
tipi2004 <socduyhoang@gmail.com>
2025-03-11 09:10:15 +0700

    Add files via upload

---
+++ ViduHelloWordserviet/src/main/java/vn/edu/til/ViduServlet/HelloWor.java
@@ -0,0 +1,33 @@
+package vn.edu.til.ViduServlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class HelloWor
+ */
+@WebServlet("/HelloWor")
+public class HelloWor extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWor() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter wt = response.getWriter();
+		wt.println("Hello Word");
+	}
+
+}
