b1e4d10795ac7c79e9321fe7b74bcbf1bb7154a8
tipi2004 <socduyhoang@gmail.com>
2025-03-25 10:56:49 +0700

    Thêm file vào thư mục TongHopGK

---
+++ TongHopGK_64131798/src/test/java/phongth/TongHopGk64131798ApplicationTests.java
@@ -0,0 +1,13 @@
+package phongth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TongHopGk64131798ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
