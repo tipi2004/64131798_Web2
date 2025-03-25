7e7d24084f1cf04fc7cf7d573a9b7e678f7317e3
tipi2004 <socduyhoang@gmail.com>
2025-03-25 10:59:56 +0700

    Resolve conflicts from pulling changes

---
+++ TruyenDL_64131798/src/main/java/phongth/TruyenDl64131798Application.java
@@ -0,0 +1,13 @@
+package phongth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruyenDl64131798Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruyenDl64131798Application.class, args);
+	}
+
+}
